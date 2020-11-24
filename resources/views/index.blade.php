7697c1a6b0d15ff1e078a59c9358606eb4e0834c
Francisco J. Colón Rodríguez <fjcr1018@gmail.com>
2020-11-24 16:33:48 -0600

    Google Analytics implementation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,6 +29,16 @@
      ================================================== -->
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L5E7KV0P8P"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-L5E7KV0P8P');
+    </script>
+
 </head>
 
 <body id="top">
